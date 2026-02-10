3ce293d4ef9fb9aa72b8d6590ebef5ba00486e97
Rajendra Chaudhary <162672656+rajendrachy@users.noreply.github.com>
2026-02-10 18:22:34 +0530

    Fix import path for userDataContext

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react'
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 
 function Card({image}) {
   
