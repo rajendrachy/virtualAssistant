94702d1493605cfac58b369a99761859165f2f48
Rajendra Chaudhary <162672656+rajendrachy@users.noreply.github.com>
2026-02-10 18:20:00 +0530

    Fix import case for userDataContext

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -11,7 +11,7 @@ import { RiImageAddLine } from "react-icons/ri";
 import { useState } from 'react';
 import { useRef } from 'react';
 import { useContext } from 'react';
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 
